6c4952aea1955dcc654b2d134dd2d3394b3eb92f
dushmanta-tanty <dushmanta.tanty@gmail.com>
2024-12-10 20:11:48 +0530

    checking with mvn assembly

---
+++ src/main/java/com/example/MyAppServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppServiceApplication.class, args);
+	}
+}
